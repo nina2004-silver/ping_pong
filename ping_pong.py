41f5d12b3be5923d4a3389385105a954f12f88f7
nina2004-silver <ninaturulina4@gmail.com>
2025-06-01 11:41:54 +0700

    Update ping_pong.py
    v1.1
    
    Добавлен класс Player

--- ping_pong.py
+++ ping_pong.py
@@ -69,24 +69,6 @@ while game:
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
             speed_y *= 1
-        
-        #если мяч достигает границ экрана, меняем направление его движения
-        if ball.rect.y > win_height-50 or ball.rect.y < 0:
-            speed_y *= -1
-
-
-        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1, (200, 200))
-            game_over = True
-
-
-        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-        if ball.rect.x > win_width:
-            finish = True
-            window.blit(lose2, (200, 200))
-            game_over = True
 
 
         racket1.reset()
